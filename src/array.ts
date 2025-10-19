1e4696b957af406affbc2ef85b3fbfd39ba3ca30
Tohru Furusawa <tohru.furusawa@gmail.com>
2025-10-19 21:56:09 +0900

    final commit

---
+++ src/array.ts
@@ -0,0 +1,16 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1, 2, 3];
+
+let strings2: Array<string> = ['Tokyo', 'Osaka', 'Kyoto'];
+
+let strings: string[] = ['JavaScript', 'TypeScript', 'CoffeeScript'];
+
+let nijigenhairestsu: number[][] = [
+  [50, 100],
+  [150, 300],
+];
+
+let hairetsu: (string | number | boolean)[] = [1, false, 'Japan'];
